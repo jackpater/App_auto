9040ebc3b988b62c64871fa5b0e141a6ee2d8a06
jacky <jackpater666@gmail.com>
2021-02-01 22:16:57 +0800

    fix bug

--- onlyOneExecute.js
+++ onlyOneExecute.js
@@ -15,9 +15,9 @@ async function start() {
     console.log(`北京时间 (UTC+08)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}}`);
     if (process.env.JD_COOKIE) {
         console.log(`当前共${process.env.JD_COOKIE.split("&").length}个账号需要签到`);
-    } else {
-        console.log("请填写 JD_COOKIE 后在继续");
-    }
+    } else //{
+        //console.log("请填写 JD_COOKIE 后在继续");
+    //}
     if (!process.env.SYNCURL) {
         console.log("请填写 SYNCURL 后在继续");
         return;
