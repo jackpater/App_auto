988dccb1d9f44b2cfe39145368f9b1400cf5749c
jacky <jackpater666@gmail.com>
2021-02-26 21:57:00 +0800

    1

--- QQreadCOOKIE.js
+++ QQreadCOOKIE.js
@@ -5,32 +5,9 @@ let QQreadurlVal = ``
 
 
 let QQreadurlcookie = {
-        "Accept": "*/*",
-        "qrem": "0",
-        "qrtm": "1614341506",
-        "ua": "",
-        "rcmd": "1",
-        "Accept-Encoding": "gzip",
-        "net_type": "1",
-        "platform": "ioswp",
-        "youngerMode": "0",
-        "loginType": "2",
-        "text_type": "1",
-        "version": "qqreader_7.5.70.0354_iphone",
-        "Accept-Language": "zh-cn",
-        "User-Agent": "QQReaderUI/2558 CFNetwork/1206 Darwin/20.1.0",
-        "ywkey": "ywk9PODQJna5",
-        "qimei": "",
-        "auditStatus": "0",
-        "Host": "commontgw6.reader.qq.com",
-        "ywguid": "120205744148",
-        "Cache-Control": "no-cache",
-        "server_sex": "1",
-        "themeid": "2000",
-        "Connection": "keep-alive",
-        "IDFA": "",
-        "nosid": "1",
-        "gselect": "-1",
+
+    QQreadurlVal: QQreadurlVal,
+
 }
 
 module.exports = QQreadurlcookie
